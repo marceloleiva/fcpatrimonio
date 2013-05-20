2e1e7910b7e7e191c5057e2f1122810fac224e2f
Marcelo leiva <chelitoleiva@gmail.com>
2013-05-20 18:30:39 -0400

    Se repara login y se cambia formato de fecha en actividades

--- frontend/views.py
+++ frontend/views.py
@@ -19,9 +19,9 @@ def login(request):
     form_login = login_form.is_valid()
 
     if form_login:
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
+        user = authenticate(username=email, password=password)
 
         if user:
             auth_login(request, user)
